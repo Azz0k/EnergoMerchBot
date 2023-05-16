c28c127ab417356b179b05912f62e0834af6e27d
Милокум Павел <pmilokum@bk.ru>
2023-05-16 12:43:38 +0300

    refactored support module

--- EnergoMerchBot.py
+++ EnergoMerchBot.py
@@ -8,41 +8,11 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from imports.utils import *
 from imports.config import *
 from imports.TwoWayDict import TwoWayDict
+from imports.Support import Support
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-folder = WORK_FOLDER
-data_frame = None
-find_trie = Trie()
-GNS = TwoWayDict()
-
-
-def update_data_frame(start: bool = False) -> None:
-    """Read data from base file and store it to the trie and to the pandas dataFrame"""
-
-    def log_and_exit(message: str) -> None:
-        logging.log(logging.CRITICAL, message)
-        if start:
-            sys.exit(message)
-
-    global data_frame
-    global find_trie
-    global GNS
-    files = []
-    new_find_trie = Trie()
-    root = None
-    for root, dirs, files in os.walk(folder):
-        break
-    for file in files:
-        if file.endswith('.xlsx'):
-            try:
-                data_frame = get_data_from_file(os.path.join(root, file), new_find_trie, GNS)
-                find_trie = new_find_trie
-            except OSError:
-                log_and_exit('Base file not accessible!!!')
-            except KeyError:
-                log_and_exit('Incorrect base file structure')
-            break
+support = Support()
 
 
 async def on_bot_start_up(dispatcher: Dispatcher) -> None:
@@ -52,33 +22,13 @@ async def on_bot_start_up(dispatcher: Dispatcher) -> None:
 async def background_job_refresh_data() -> None:
     while True:
         await asyncio.sleep(WORKER_DELAY_IN_SECONDS)
-        update_data_frame()
+        support.update_data_frame()
 
 
 @dp.message_handler(commands=['start'])
 async def show_start(message: types.Message):
-    new_first_markup = create_standard_markup('', find_trie.get_children(''))
+    new_first_markup = support.create_standard_markup('')
     await message.answer(text='Выберите свою территорию или пришлите ее название', reply_markup=new_first_markup)
-
-
-def create_reply_markup(data: str) -> Any:
-    """Return an inline keyboard markup with one button"""
-    reply_markup = InlineKeyboardMarkup()
-    button = InlineKeyboardButton(text='Повторить последний запрос', callback_data=f'{BUTTON_PREFIX}{data}')
-    reply_markup.add(button)
-    return reply_markup
-
-
-def create_standard_markup(query: str, children) -> Any:
-    markup = InlineKeyboardMarkup(row_width=3)
-    for i in range(0, len(children)):
-        element = children[i]
-        button = InlineKeyboardButton(text=element, callback_data=f'{BUTTON_PREFIX}{query}_{GNS.get_id_from_name(element)}')
-        if i % 3 == 0:
-            markup.add(button)
-        else:
-            markup.insert(button)
-    return markup
 
 
 @dp.callback_query_handler()
@@ -90,31 +40,30 @@ async def process_callbacks(callback_query: types.CallbackQuery):
                                text='Устаревшая версия кнопок, сотрите историю и начните заново')
         return
     query = callback_query.data[len(BUTTON_PREFIX):].strip('_')
-    children = find_trie.get_children(GNS.replace_ids_with_names(query))
+    children = support.get_children(query)
     if len(children) > 0:
-        new_markup = create_standard_markup(query, children)
-        await bot.send_message(callback_query.from_user.id, text=GNS.replace_ids_with_names(query), reply_markup=new_markup)
+        markup = support.create_standard_markup(query)
+        text = support.replace_ids_with_names(query)
+        await bot.send_message(callback_query.from_user.id, text=text, reply_markup=markup)
     else:
-        reply_markup = create_reply_markup(query)
-        territory_index = find_trie.get_index(GNS.replace_ids_with_names(query))
-        data_row_list = data_frame.loc[territory_index, :].values.tolist()[1:]
-        result = convert_to_string(GNS.replace_ids_with_names(query), data_row_list)
-        await bot.send_message(callback_query.from_user.id, text=result, parse_mode='HTML', reply_markup=reply_markup)
+        text = support.get_answer(query)
+        markup = support.create_reply_markup(query)
+        await bot.send_message(callback_query.from_user.id, text=text, parse_mode='HTML', reply_markup=markup)
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
     """Handling a response to a text query"""
-    reply_markup = create_reply_markup(GNS.replace_names_with_ids(message.text))
-    territory_index = find_trie.get_index(message.text)
-    data_row_list = data_frame.loc[territory_index, :].values.tolist()[1:]
-    result = convert_to_string('', data_row_list)
-    await message.answer(text=result, parse_mode='HTML', reply_markup=reply_markup)
+    markup = InlineKeyboardMarkup()
+    text = support.get_answer(message.text.strip())
+    if text != NOT_FOUND_ANSWER:
+        markup = support.create_reply_markup(message.text)
+    await message.answer(text=text, parse_mode='HTML', reply_markup=markup)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s %(name)s %(levelname)s %(message)s',
                         datefmt='%H:%M:%S')
-    update_data_frame(start=True)
+    support.update_data_frame(start=True)
     executor.start_polling(dp, skip_updates=True, on_startup=on_bot_start_up)
--- imports/Support.py
+++ imports/Support.py
@@ -32,10 +32,12 @@ class Support:
 
     def update_data_frame(self, start: bool = False) -> None:
         """Read data from base file and store it to the trie and to the pandas dataFrame"""
+
         def log_and_exit(message: str) -> None:
             logging.log(logging.CRITICAL, message)
             if start:
                 sys.exit(message)
+
         files = []
         root = None
         for root, dirs, files in os.walk(WORK_FOLDER):
@@ -50,16 +52,21 @@ class Support:
                     log_and_exit('Incorrect base file structure')
                 break
 
-    @staticmethod
-    def create_reply_markup(data: str) -> Any:
+    def create_reply_markup(self, data: str) -> Any:
         """Return an inline keyboard markup with one button"""
         reply_markup = InlineKeyboardMarkup()
+        if data[0:1].isalpha():
+            data = self.name_vs_id.replace_names_with_ids(data)
         button = InlineKeyboardButton(text='Повторить последний запрос', callback_data=f'{BUTTON_PREFIX}{data}')
         reply_markup.add(button)
         return reply_markup
 
-    def create_standard_markup(self, query: str, children) -> Any:
+    def create_standard_markup(self, query: str) -> Any:
         markup = InlineKeyboardMarkup(row_width=3)
+        callback_id = query
+        if query[0:1].isdigit():
+            callback_id = self.name_vs_id.replace_ids_with_names(query)
+        children = self.find_trie.get_children(callback_id)
         for i in range(0, len(children)):
             element = children[i]
             button = InlineKeyboardButton(text=element,
@@ -77,10 +84,17 @@ class Support:
         return self.find_trie.get_index(self.name_vs_id.replace_ids_with_names(query))
 
     def get_answer(self, query: str) -> Any:
+        territory_name = query
         if query[0:1].isalpha():
             territory_index = self.find_trie.get_index(query)
+            if territory_index == -1:
+                return NOT_FOUND_ANSWER
         else:
             territory_index = self.find_trie.get_index(self.name_vs_id.replace_ids_with_names(query))
+            territory_name = self.name_vs_id.replace_ids_with_names(query)
         data_row_list = self.data_frame.loc[territory_index, :].values.tolist()[1:]
-        result = convert_to_string(self.name_vs_id.replace_ids_with_names(query), data_row_list)
+        result = convert_to_string(territory_name, data_row_list)
         return result
+
+    def replace_ids_with_names(self, query: str):
+        return self.name_vs_id.replace_ids_with_names(query)
